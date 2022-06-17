aa04766d979c0e71894a63b4f6a509bed4f84337
Javi <49827915+Javierxzxz@users.noreply.github.com>
2022-06-16 22:25:44 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,94 @@
+function americano() {
+    sum = document.getElementById('output');
+    sum.value = parseFloat(sum.value) + 0.75 + " $";
+}
+
+function cafe__leche() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 0.75 + " $";
+}
+
+function cafe__black() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 0.50 + " $";
+}
+
+function latte() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 1.25 + " $";
+}
+
+function ristretto() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 1.00 + " $";
+}
+
+function capuchino() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 1.50 + " $";
+}
+
+function cafe__helado() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 0.50 + " $";
+}
+
+function frapuchino__leche() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 1.75 + " $";
+}
+
+function te__verde() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 1.25 + " $";
+}
+
+function frapuchino() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 1.50 + " $";
+}
+
+function te__leche() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 1.00 + " $";
+}
+
+function batido() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 1.50 + " $";
+}
+
+function helado() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 1.50 + " $";
+}
+
+function pastel__cafe() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 1.75 + " $";
+}
+
+function barras__chocolate() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 1.25 + " $";
+}
+
+function brownies() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 1.00 + " $";
+}
+
+function pastel__chocolate() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 1.75 + " $";
+}
+
+function tarta() {
+    sum = document.getElementById("output");
+    sum.value = parseFloat(sum.value) + 1.60 + " $";
+}
+
+function reset() {
+    delet = document.getElementById("output");
+    delet.value = "0.00";
+}
